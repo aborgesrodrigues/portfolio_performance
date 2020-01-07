aa20728f3e8d2308f4ca852dc7bbdf25595e71cc
alessandro.rodrigues <alessandro.rodrigues@ifto.edu.br>
2020-01-07 12:06:45 -0300

    set order by to the performance query

--- historical_performance/views.py
+++ historical_performance/views.py
@@ -55,7 +55,7 @@ def portfolio_view(request, username= None):
 			formset.save()
 
 			#Save daily historical performance data
-			for allocation in initial_portfolio.allocations.all().order_by("date"):
+			for allocation in initial_portfolio.allocations.all():
 				#remove old allocations
 				PerformancePortfolio.objects.filter(allocation=allocation).delete()
 
@@ -87,7 +87,7 @@ def portfolio_view(request, username= None):
 
 	if initial_portfolio:
 
-		for allocation in initial_portfolio.allocations.all():
+		for allocation in initial_portfolio.allocations.all().order_by("date"):
 			#stock_price_history_day_data[allocation.stock] = [dict(x=performance.date.strftime("%Y-%m-%d"), y=str(performance.unit_value)) for performance in allocation.performances.all()]
 
 			month_data[allocation.stock] = {}
