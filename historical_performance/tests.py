86dcbfeb6c342b72e11103a2c5f7c6653024c75a
alessandro.rodrigues <alessandro.rodrigues@ifto.edu.br>
2020-01-20 16:04:10 -0300

    add more test cases

--- historical_performance/tests.py
+++ historical_performance/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+import datetime
 
 # Create your tests here.
-from historical_performance.forms import PortfolioForm, AllocationForm
+from historical_performance.forms import PortfolioForm, AllocationForm, AllocationFormSet
 from historical_performance.models import Portfolio
 
 
@@ -33,43 +34,44 @@ class PortfolioFormTests(TestCase):
 		})
 
 
-
 class AllocationTests(TestCase):
 	def setUp(self):
 		self.portfolio = Portfolio()
 		self.portfolio.username = "aborges"
-		self.portfolio.start_date = "2016-01-01"
+		self.portfolio.start_date = datetime.datetime.strptime("2016-01-01", "%Y-%m-%d")
 		self.portfolio.initial_balance = 10000
 		self.portfolio.residual = 0
 		self.portfolio.save()
 
+		self.empty_data = {"allocations-0-portfolio": "",
+		        "allocations-0-stock": "",
+		        "allocations-0-percentage": "",
+		        "allocations-0-unit_value": "",
+		        "allocations-0-quantity": "",
+		        "allocations-0-total": ""}
+
+		self.data = {"allocations-0-portfolio": self.portfolio,
+			      "allocations-0-stock": "SNAP",
+			      "allocations-0-percentage": 100,
+			      "allocations-0-unit_value": 10,
+			      "allocations-0-quantity": 10,
+			      "allocations-0-total": 100}
+
 	# Valid Form Data
 	def test_valid(self):
 		form = AllocationForm(
-			data={"allocations-0-portfolio": self.portfolio, 
-			      "allocations-0-stock": "SNAP", 
-			      "allocations-0-percentage": 100, 
-			      "allocations-0-unit_value": 10, 
-			      "allocations-0-quantity": 10,
-			      "allocations-0-total": 10},
+			data= self.data,
 			prefix= "allocations-0")
 		self.assertTrue(form.is_valid(), "Should be valid")
-		print(form.errors)
 
 		allocation = form.save()
 		self.assertTrue(allocation.pk is not None, "Allocation saved by form")
 
-	# Invalid Form Data
-	def test_invalid(self):
-		#validate required fields
-		data = {"allocations-0-portfolio": "", 
-		        "allocations-0-stock": "", 
-		        "allocations-0-percentage": "", 
-		        "allocations-0-unit_value": "",
-		        "allocations-0-quantity": "",
-		        "allocations-0-total": ""}
+	#Required fields
+	def test_required_fields(self):
+
 		form = AllocationForm(
-			data=data,
+			data=self.empty_data,
 			prefix= "allocations-0")
 		self.assertFalse(form.is_valid(), "Fields required.")
 
@@ -82,30 +84,26 @@ class AllocationTests(TestCase):
 			"total": ["This field is required."],
 		})
 
-		#validate stock is valid
+	# Stock is valid
+	def test_valid_stock(self):
+		data = self.data
+		data["allocations-0-stock"] = "SNAP11"
 		form = AllocationForm(
-			{"allocations-0-portfolio": self.portfolio,
-			 "allocations-0-stock": "SNAP11",
-			 "allocations-0-percentage": 100,
-			 "allocations-0-unit_value": 10,
-			 "allocations-0-quantity": 10,
-			 "allocations-0-total": 10},
+			data = data,
 			prefix= "allocations-0")
-		self.assertFalse(form.is_valid())
+		self.assertFalse(form.is_valid(), "Should be invalid")
 
 		self.assertEqual(form.errors, {
 			"stock":  ["Stock symbol is not valid."],
 			"__all__": ["Stock symbol is not valid."]
 		})
 
-		#validate percentage is valid
+	#Percentage is in a valid range
+	def test_valid_percentage(self):
+		data = self.data
+		data["allocations-0-percentage"] = 200
 		form = AllocationForm(
-			{"allocations-0-portfolio": self.portfolio,
-			 "allocations-0-stock": "SNAP",
-			 "allocations-0-percentage": 200,
-			 "allocations-0-unit_value": 10,
-			 "allocations-0-quantity": 10,
-			 "allocations-0-total": 10},
+			data= data,
 			prefix= "allocations-0")
 		self.assertFalse(form.is_valid())
 
@@ -113,3 +111,70 @@ class AllocationTests(TestCase):
 			"percentage":  ["The percentage should be between 0 and 100."]
 		})
 
+	#Total is equal unit_value x quantity
+	def test_valid_total(self):
+		data = self.data
+		data["allocations-0-total"] = 200
+		form = AllocationForm(
+			data= data,
+			prefix= "allocations-0")
+		self.assertFalse(form.is_valid())
+
+		correct_total = self.data.get("allocations-0-unit_value") * self.data.get("allocations-0-quantity")
+
+		self.assertEqual(form.errors, {
+			"total":  ["The total should be %s, %s found." % (correct_total, self.data.get("allocations-0-total"))]
+		})
+
+
+class AllocationFormSetTests(TestCase):
+
+	def setUp(self):
+		self.portfolio = Portfolio()
+		self.portfolio.username = "aborges"
+		self.portfolio.start_date = datetime.datetime.strptime("2016-01-01", "%Y-%m-%d")
+		self.portfolio.initial_balance = 10000
+		self.portfolio.residual = 0
+		self.portfolio.save()
+
+		self.empty_data = {
+			'allocations-TOTAL_FORMS': 0,
+			'allocations-INITIAL_FORMS': 0,
+			'allocations-MIN_NUM_FORMS': 1,
+			'allocations-MAX_NUM_FORMS': 1000
+		}
+
+		self.data = {
+            'allocations-0-portfolio': self.portfolio,
+            'allocations-0-stock': "SNAP",
+            'allocations-0-percentage': 50,
+            'allocations-0-unit_value': 10,
+            'allocations-0-quantity': 10,
+			'allocations-0-total': 100,
+			'allocations-TOTAL_FORMS': 2,
+			'allocations-1-portfolio': self.portfolio,
+			'allocations-1-stock': "SNAP",
+			'allocations-1-percentage': 60,
+			'allocations-1-unit_value': 20,
+			'allocations-1-quantity': 10,
+			'allocations-1-total': 200,
+			'allocations-INITIAL_FORMS': 0,
+			'allocations-MIN_NUM_FORMS': 1,
+			'allocations-MAX_NUM_FORMS': 1000
+		}
+
+	#At least one form is informed
+	def test_required(self):
+		formset = AllocationFormSet(instance= self.portfolio, data=self.empty_data)
+
+		self.assertFalse(formset.is_valid())
+
+		self.assertEqual(formset.non_form_errors(), ["Inform at least one stock."])
+
+	#Total of percentages is in a valid rante
+	def test_total_percentage(self):
+		formset = AllocationFormSet(instance= self.portfolio, data=self.data)
+
+		self.assertFalse(formset.is_valid())
+
+		self.assertEqual(formset.non_form_errors(), ["The sum of the percentages should not be more than 100."])
